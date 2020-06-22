1e692b40b2b5d9082dc30a0dfe704f55d4bf670f
Travis Ricks <travis@travisricks.com>
2020-06-22 15:08:41 +0900

    add favicon and needed plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,18 +1,30 @@
 module.exports = {
   siteMetadata: {
-    title: 'gatsby-starter-code-notes',
-    description: `A starter for gatsby-theme-code-notes`,
-    author: 'Zander',
+    title: "Travis Ricks Code Notes",
+    description: `Travis Ricks Code Notes`,
+    author: "Travis Ricks",
   },
   plugins: [
     {
-      resolve: 'gatsby-theme-code-notes',
+      resolve: "gatsby-theme-code-notes",
       options: {
-        contentPath: 'notes',
-        basePath: '/',
+        contentPath: "notes",
+        basePath: "/",
         showThemeInfo: true,
         showDescriptionInSidebar: true,
       },
     },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Travis Ricks Code Notes`,
+        short_name: `TRCode`,
+        start_url: `/`,
+        background_color: `#f7f0eb`,
+        theme_color: `#a2466c`,
+        display: `standalone`,
+        icon: `static/logo.png`,
+      },
+    },
   ],
-}
+};
